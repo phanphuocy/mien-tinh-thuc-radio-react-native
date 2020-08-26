978634004efebf1f2f6b0577846a1eb3bf077a00
Phan Phuoc Y <phanphuocy@gmail.com>
2020-08-26 21:48:04 +0700

    commit with git lfs extension

---
+++ navigators/BrowsingNavigator.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Text, View, Button } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+      <Button
+        title="Open player"
+        onPress={() => navigation.navigate("Player")}
+      />
+    </View>
+  );
+}
+
+function DownloadedScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Downloaded!</Text>
+    </View>
+  );
+}
+
+const BrowsingTabs = createMaterialBottomTabNavigator();
+
+function BrowsingNavigator() {
+  return (
+    <BrowsingTabs.Navigator
+      labeled={false}
+      barStyle={{ backgroundColor: "white", paddingVertical: 8 }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = "md-home";
+          } else if (route.name === "Downloaded") {
+            iconName = "md-download";
+          }
+          return <Ionicons name={iconName} size={28} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <BrowsingTabs.Screen name="Home" component={HomeScreen} />
+      <BrowsingTabs.Screen name="Downloaded" component={DownloadedScreen} />
+    </BrowsingTabs.Navigator>
+  );
+}
+
+export default BrowsingNavigator;
---
+++ navigators/RootNavigator.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// Import screens
+import PlayerScreen from "../screens/PlayerScreen";
+
+// Import navigator
+import BrowsingNavigator from "./BrowsingNavigator";
+
+const RootStack = createStackNavigator();
+
+function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Browsing" component={BrowsingNavigator} />
+        <RootStack.Screen name="Player" component={PlayerScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default RootNavigator;
